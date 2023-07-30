32996a57d2b6edbfb141e108f5c810a374339389
Protato64 <jack.micher26@gmail.com>
2023-07-29 21:13:57 -0400

    add NOT functionality to logicalOperators lesson

--- logicalOperators.js
+++ logicalOperators.js
@@ -6,6 +6,9 @@ let result = x < y && x < z;
 
 console.log(result);
 
+// Will always be the opposite boolean value of result
+console.log(!result)
+
 // && is AND
 // || is OR
 // Preceding boolean value with ! results in NOT
