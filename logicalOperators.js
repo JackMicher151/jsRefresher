5cb542a4a51441ed886c7412768899024909a8f4
Protato64 <jack.micher26@gmail.com>
2023-07-29 21:12:41 -0400

    comment logicalOperators.js

--- logicalOperators.js
+++ logicalOperators.js
@@ -1,5 +1,11 @@
 let x = 7, y = 8, z = 9;
 
+
+// (x < y) AND (x < 7), will be true
 let result = x < y && x < z;
 
 console.log(result);
+
+// && is AND
+// || is OR
+// Preceding boolean value with ! results in NOT
