3950b9030924ef26e0b5b1d2d66b62c940eeb0cf
Jack Micher <jack.micher26@gmail.com>
2024-04-30 10:37:51 -0400

    Create interactive FizzBuzz page

--- odin-project-learning/lesson6fizzbuzzpage/script.js
+++ odin-project-learning/lesson6fizzbuzzpage/script.js
@@ -1,14 +1,14 @@
 function fizzbuzz(num) {
     let rstr = '';
     if (num % 3 === 0) {
-        rstr.concat('Fizz');
+        rstr = rstr.concat('Fizz');
     }
     if (num % 5 === 0) {
-        rstr.concat('Buzz');
+        rstr = rstr.concat('Buzz');
     }
     return rstr;
 }
 
-document.querySelector('button.butt1').addEventListener("click", () => {
-    alert(fizzbuzz(document.querySelector("input.epic").value));
+document.querySelector('#butt1').addEventListener("click", () => {
+    alert(fizzbuzz(document.querySelector("#epic").value));
 })
