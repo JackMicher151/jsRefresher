d7221882432f5869894a6c20bc647b1c4284a6b3
Jack Micher <jack.micher26@gmail.com>
2024-05-01 22:25:19 -0400

    add getComputerChoice and getPlayerChoice functions

--- odin-project-learning/lesson7rps/script.js
+++ odin-project-learning/lesson7rps/script.js
@@ -1,7 +1,29 @@
-let playerChoice = 0;
-let cpuChoice = 0;
+// We will learn about closures later to make something like this private
+// We will learn about currying functions later, just because
+
+function getComputerChoice() {
+    let cpuChoice = Math.floor(Math.random(3));
+    switch (cpuChoice) {
+        case 0:
+            return 'ROCK';
+        case 1:
+            return 'PAPER';
+        case 2:
+            return 'SCISSORS';
+    }
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter your move (Rock, Paper, or Scissors").toString();
+    console.log('Player Chose ' + playerChoice);
+    playerChoice = playerChoice.toUpperCase();
+    return (playerChoice === 'ROCK' || playerChoice === 'PAPER' || playerChoice === 'SCISSORS') ? playerChoice : getPlayerChoice();
+}
 
 function playRound() {
+    compSelection = getComputerChoice();
+    playerSelection = getPlayerChoice();
+
     
 }
 
