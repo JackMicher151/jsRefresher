b3eda69b91989e47026047c005b2878e29dea9cc
Jack Micher <jack.micher26@gmail.com>
2024-05-03 16:49:22 -0400

    Layout styles and add constants for ASCII hand characters

--- odin-project-learning/lesson7rps/script.js
+++ odin-project-learning/lesson7rps/script.js
@@ -1,5 +1,8 @@
 // We will learn about closures later to make something like this private
 // We will learn about currying functions later, just because
+const rockchar = '&#9994';
+const paperchar = '&#9995';
+const scisschar = '&#9996';
 
 function getComputerChoice() {
     let cpuChoice = Math.floor(Math.random(3));
@@ -21,9 +24,24 @@ function getPlayerChoice() {
 }
 
 function playRound() {
-    compSelection = getComputerChoice();
-    playerSelection = getPlayerChoice();
+    let compSelection = getComputerChoice();
+    let playerSelection = getPlayerChoice();
+    let result;
 
-    
+    if (compSelection === playerSelection) {
+        result = 'Tie'
+    }
+    else {
+        switch (playerSelection) {
+            case 'ROCK':
+                result = compSelection === 'SCISSORS' ? 'You Win!' : 'You Lose...';
+            case 'PAPER':
+                result = compSelection === 'ROCK' ? 'You Win!' : 'You Lose...';
+            case 'SCISSORS':
+                result = compSelection === 'PAPER' ? 'You Win!' : 'You Lose...';
+        }
+    }
+
+    return result;
 }
 
